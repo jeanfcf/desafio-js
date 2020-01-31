7439eccdf48f8a5fc669f7b6edf0f468d2934089
Jean Coelho <felipe_jean80@hotmail.com>
2020-01-31 04:12:02 -0300

    Alerta personalizado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
 
 function cadastro(){
-    alert("Cadastre-se")
+    Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'Proximo &rarr;',
+        showCancelButton: true,
+        progressSteps: ['1', '2', '3'],
+        cancelButtonText: 'Cancelar'
+      }).queue([
+        {
+          title: 'Cadastre-se para receber mais informacoes',
+          text: 'Insira seu nome:'
+        },
+        'Agora insira seu e-mail:',
+        'Crie sua nova senha:'
+      ]).then((result) => {
+        if (result.value) {
+          const answers = JSON.stringify(result.value)
+          Swal.fire({
+            title: 'Pronto!',
+            confirmButtonText: 'Prosseguir para o site'
+          })
+        }
+      })
 }
+
