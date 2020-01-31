6c2282fb8391415f829fd81e8dd2bc658cece8cd
Jean Coelho <felipe_jean80@hotmail.com>
2020-01-31 08:59:00 -0300

    get ajeitado

--- js/main.js
+++ js/main.js
@@ -1,134 +1,25 @@
 
 $(document).ready(function(){
-  
-          var urlStr = "https://api.punkapi.com/v2/beers/random";
-
-          
-          $.ajax({
-              
-              url : urlStr,
-              type : "get",
-              dataType : "json",
-              success : function(data){
-                  console.log(data);
-                  console.log(
-                    data[0].name)
-                  $("#titulo1").text(data[0].name);
-                  $("#fabricacao1").text(data[0].first_brewed);
-                  $("#slogan1").text(data[0].description);
-                  $("#alcool1").text(data[0].ph);
-                  $("#card1").css({
-                    'background-image':'url('+data[0].image_url + ')',
-                    'background-size': 'contain',
-                    'background-repeat': 'no-repeat',
-                    'background-position': 'center'
-                  });
-              },
-              error : function(erro){
-                  console.log(erro);
-                  console.log("errooo");
-              }
-          });
-          $.ajax({
-            url : urlStr,
-            type : "get",
-            dataType : "json",
-            success : function(data){
-                console.log(data);
-                console.log(
-                  data[0].name)
-                $("#titulo2").text(data[0].name);
-                $("#fabricacao2").text(data[0].first_brewed);
-                $("#slogan2").text(data[0].description);
-                $("#alcool2").text(data[0].ph);
-                $("#card2").css({
-                  'background-image':'url('+data[0].image_url + ')',
-                  'background-size': 'contain',
-                  'background-repeat': 'no-repeat',
-                  'background-position': 'center'
-                });
-            },
-            error : function(erro){
-                console.log(erro);
-                console.log("errooo");
-            }
-        });
-        $.ajax({
-          url : urlStr,
-          type : "get",
-          dataType : "json",
-          success : function(data){
-              console.log(data);
-              console.log(
-                data[0].name)
-              $("#titulo3").text(data[0].name);
-              $("#fabricacao3").text(data[0].first_brewed);
-              $("#slogan3").text(data[0].description);
-              $("#alcool3").text(data[0].ph);
-              $("#card3").css({
-                'background-image':'url('+data[0].image_url + ')',
-                'background-size': 'contain',
-                'background-repeat': 'no-repeat',
-                'background-position': 'center'
-              });
-          },
-          error : function(erro){
-              console.log(erro);
-              console.log("errooo");
-          }
-      });
-      $.ajax({
-        url : urlStr,
-        type : "get",
-        dataType : "json",
-        success : function(data){
-            console.log(data);
-            console.log(
-              data[0].name)
-            $("#titulo4").text(data[0].name);
-            $("#fabricacao4").text(data[0].first_brewed);
-            $("#slogan4").text(data[0].description);
-            $("#alcool4").text(data[0].ph);
-            $("#card4").css({
-              'background-image':'url('+data[0].image_url + ')',
-              'background-size': 'contain',
-              'background-repeat': 'no-repeat',
-              'background-position': 'center'
-            });
-        },
-        error : function(erro){
-            console.log(erro);
-            console.log("errooo");
-        }
-    });
+  var urlStr = "https://api.punkapi.com/v2/beers/random";
+  for (let index = 1; index < 6; index++) {
     $.ajax({
       url : urlStr,
       type : "get",
       dataType : "json",
       success : function(data){
-          console.log(data);
-          console.log(
-            data[0].name)
-          $("#titulo5").text(data[0].name);
-          $("#fabricacao5").text(data[0].first_brewed);
-          $("#slogan5").text(data[0].description);
-          $("#alcool5").text(data[0].ph);
-          $("#card5").css({
-            'background-image':'url('+data[0].image_url + ')',
-            'background-size': 'contain',
-            'background-repeat': 'no-repeat',
-            'background-position': 'center'
-          });
+        console.log(data);
+        $("#titulo"+index).text(data[0].name);
+        $("#fabricacao"+index).text(data[0].first_brewed);
+        $("#slogan"+index).text(data[0].description);
+        $("#alcool"+index).text(data[0].ph);
+        $("#card"+index).css({'background-image':'url('+data[0].image_url + ')',});
       },
       error : function(erro){
-          console.log(erro);
-          console.log("errooo");
+        console.log(erro);
       }
-  });
-
-
-  });
-
+    });
+  }
+});
 
 
 window.onload = function(){
@@ -157,7 +48,4 @@ function cadastro(){
           })
         }
       })
-}
-
-
-
+    }
